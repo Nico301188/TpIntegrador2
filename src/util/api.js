9a8a8e68f5ad8a7cad4bf7eb4e277bc1fb25d057
Nico301188 <nico301188@gmail.com>
2024-05-30 18:42:01 -0300

    rout messages

--- src/util/api.js
+++ src/util/api.js
@@ -3,21 +3,22 @@ import axios from "axios";
 const axiosInstance = axios.create({
     // baseURL: "https://65f8d55edf15145246101010.mockapi.io/api/"
     // baseURL: "https://66044fc62393662c31d126a2.mockapi.io/"
-    baseURL: process.env.REACT_APP_BASE_URL_API
+    baseURL: //"mongodb+srv://nico301188:EduIt2024@products.kjpndol.mongodb.net/products"
+     process.env.REACT_APP_BASE_URL_API
 })
 
 export const getProducts = async () => {
     // const resp = await axiosInstance.get("/products")
-    const resp = await axiosInstance.get("/juegos")
+    const resp = await axiosInstance.get("/products")
     return resp.data;
 }
 
 export const postMessage = async body => {
-    const resp = await axiosInstance.post("/Contact", body)
+    const resp = await axiosInstance.post("/messages", body)
     return resp.data;
 } 
 
 export const postProducts = async body => {
-    const resp = await axiosInstance.post("/juegos", body)
+    const resp = await axiosInstance.post("/products", body)
     return resp.data;
 } 
