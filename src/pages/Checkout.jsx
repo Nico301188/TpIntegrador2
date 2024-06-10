b73cc2073cb5e806b20d562bae1e5f75b1443708
Nico301188 <nico301188@gmail.com>
2024-06-10 14:45:59 -0300

    update checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -2,17 +2,16 @@ import React, { useContext, useEffect, useState } from 'react'
 import { Wallet } from '@mercadopago/sdk-react';
 
 import { CartContext } from '../context/CartContext';
-// import CartItem from '../components/Cart';
 
 import { postPreferenceMP } from '../util/api';
 
-function Checkout() {
+function Checkout({product, quantity}) {
     const [prefId, setPrefId] = useState("")
     const { productsCartList } = useContext(CartContext);
 
     useEffect(() => {
         console.log(productsCartList)
-        const itemsList = productsCartList.map(({product, quantity}) => ({
+        const itemsList = productsCartList.map(({ product, quantity }) => ({
             id: product._id,
             title: product.title,
             picture_url: product.imgUrl,
@@ -22,46 +21,45 @@ function Checkout() {
         postPreferenceMP({
             items: itemsList
         })
-            .then( data => setPrefId(data.preferenceId))
+            .then(data => setPrefId(data.preferenceId))
             .catch(console.log)
     }, [productsCartList])
-    
+
     return (
         <div>
             {
                 productsCartList.map(
-                    data =>
-                        // <CartItem
-                        //     key={data.product._id}
-                        //     {...data}
-                        //     edit={false}
-                        // />
-                        <div className='cart-modal__container'>
-            <img src={data.product.imgUrl} alt={data.product.sDescription} />
-            <h2>
-                {data.product.name}
-            </h2>
-                <span>({data.product.brand})</span>
-            <p>{data.product.price * data.quantity}</p>
-            <p>{data.product.category}</p>
-            <p>{data.product.freeDeliver}</p>
-            {/* {
-                edit ?
-                <Counter 
-                    _id={product._id} 
-                    product={product}
-                    initialValue={quantity}
-                    />: undefined
-            } */}
-        </div>
+                    // data =>
+                        // <div className='cart-modal__container'>
+                        //     <img src={data.product.imgUrl} alt={data.product.sDescription} />
+                        //     <h2>
+                        //         {data.product.name}
+                        //     </h2>
+                        //     <span>({data.product.brand})</span>
+                        //     <p>{data.product.price * data.quantity}</p>
+                        //     <p>{data.product.category}</p>
+                        //     <p>{data.product.freeDeliver}</p>
+                        // </div>
+                                <div className='cart-modal__container'>
+                                <img src={product.imgUrl} alt={product.sDescription} />
+                                <h2>
+                                    {product.name}
+                                </h2>
+                                <span>({product.brand})</span>
+                                <p>{product.price * quantity}</p>
+                                <p>{product.category}</p>
+                                <p>{product.freeDeliver}</p>
+                            </div>
                 )
 
             }
+            <div>
             {
                 prefId ?
-                    <Wallet initialization={{ preferenceId: prefId }} customization={{ texts:{ valueProp: 'smart_option'}}} />
-                : undefined
+                    <Wallet initialization={{ preferenceId: prefId }} customization={{ texts: { valueProp: 'smart_option' } }} />
+                    : undefined
             }
+            </div>
         </div>
     )
 }
